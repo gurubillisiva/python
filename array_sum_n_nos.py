a8daeced759ad207fcb754d998502d233576236e
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-01-07 15:43:10 +0530

    Update array_sum_n_nos.py

--- array_sum_n_nos.py
+++ array_sum_n_nos.py
@@ -1,7 +1,9 @@
 N,K=map(int,input().split())
-arr=list(map(int,input().split()))
+arr=[]
 summ=0
+for i in range(0,N):
+    a=int(input())
+    arr.append(a)
 for i in range(0,K):
-  summ=sum+arr[i]
+    summ=summ+arr[i]
 print(summ)
-  
