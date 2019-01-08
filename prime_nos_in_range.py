ba16f9d33137f987e9419dadd6898620b22ba3c3
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-01-08 16:27:03 +0530

    Update prime_nos_in_range.py

--- prime_nos_in_range.py
+++ prime_nos_in_range.py
@@ -1,11 +1,13 @@
 n,q=map(int, input().split())
 if n==1:
-	n=n+1
+    n=n+1
+x=''
 for i in range(n,q):
-	flag=0
-	for j in range(2,i//2+1):
-		if i%j==0:
-			flag=1
-			break
-	if flag==0:
-		print(i,end=' ')
+    flag=0
+    for j in range(2,i//2+1):
+        if i%j==0:
+            flag=1
+            break
+    if flag==0:
+            x=x+str(i)+' '
+print(x.strip())
