c29d29b85634c650410ccc8989b0315f299f582b
vijayaramanp <44201057+vijayaramanp@users.noreply.github.com>
2019-01-08 16:06:34 +0530

    Update two_odds_intervals.py

--- two_odds_intervals.py
+++ two_odds_intervals.py
@@ -1,8 +1,12 @@
 n,q=map(int, input().split())
 count=0
+if q%2==0:
+    q_odd=q-1
+else:
+    q_odd=q-2
 for i in range(n+1,q):
     if i%2!=0:
-        print(i,end=' ')
-        count+=1
-    if count==2:
-        break
+    	if i!=q_odd:
+    		print(i,end=' ')
+    	else:
+        	print(i)
